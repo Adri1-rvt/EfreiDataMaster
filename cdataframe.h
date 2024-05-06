e306b545483086bfcd73e828be9b6f3a4aa79c21
tessp05 <tesspoirat05@gmail.com>
2024-05-06 08:27:50 +0200

    column.c update

--- cdataframe.h
+++ cdataframe.h
@@ -1,28 +1,32 @@
 /**
  * Fichier : cdataframe.h (fichier d'en-tête de cdataframe.c)
  * Auteurs : Tess POIRAT, Adrien RIVET
- * Version : 1.1
+ * Version : 1.2
  */
 
 
+/**==========DIRECTIVES DU PREPROCESSEUR==========*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "column.h"   // inclure le fichier d'en-tête column.h
-
 #define COL_MAX 100   // constante de colonne max définie sur 100
 #define ROW_MAX 1000   // constante de ligne max définie sur 1000
 
-typedef struct {   // structure des cdataframe
-    int number_col;
-    int number_row;
-    COLUMN* columns[COL_MAX];
-} CDATAFRAME;
 
+/**==========STRUCTURES==========*/
+typedef struct {   // structure des cdataframe
+    int number_col;   // nombre de colonnes
+    int number_row;   // nombre de lignes
+    COLUMN* columns[COL_MAX];   // taableau de pointeur vers les colonnes (taille physique définie sur la constante COL_MAW)
+} CDATAFRAME;   // alias CDATAFRAME attribué à notre structure
+
+
+/**==========PROTOTYPES DES FONCTIONS==========*/
 int main2();   // prototype de la fonction de test du cdataframe
 
-// Fonction d'alimentation
+// Fonctions d'alimentation
 CDATAFRAME* create_dataframe();   // prototype de la fonction de création d'un cdataframe vide
-void fill_dataframe_by_input(CDATAFRAME* dataframe);   // prototype de la fonction de remplissage du dataframe par saisies utilisateur
+void fill_dataframe_by_input(CDATAFRAME* dataframe);   // prototype de la fonction de remplissage du dataframe par saisi utilisateur
 void fill_dataframe(CDATAFRAME* dataframe);   // prototype de la fonction de remplissage du dataframe en dur
 
 // Fonctions d'affichage
@@ -35,7 +39,7 @@ void add_row(CDATAFRAME* dataframe, int* val);   // prototype de la fonction per
 void remove_row(CDATAFRAME* dataframe, int index);   // prototype de la fonction permettant de supprimer une ligne
 void add_column(CDATAFRAME* dataframe, char* title);   // prototype de la fonction permettant d'ajouter une colonne
 void remove_column(CDATAFRAME* dataframe, int index);   // prototype de la fonction permettant de supprimer une colonne
-void rename_column(CDATAFRAME* dataframe, int index, char* new_title);   // prototype de la fonction permettant de renommer une colonne
+void rename_column(CDATAFRAME* dataframe, int index, char* title2);   // prototype de la fonction permettant de renommer une colonne
 int search_value(CDATAFRAME* dataframe, int val);   // prototype de la fonction permettant de chercher une valeur
 int get_value(CDATAFRAME* dataframe, int row, int col);   // prototype de la fonction permettant d'accéder à une valeur
 void set_value(CDATAFRAME* dataframe, int row, int col, int val);   // prototype de la fonction permettant de remplacer une valeur
